ddb76ca3d2678f44beebfba7888914a6a1ba059c
Navin <navin@webware.io>
2020-04-11 05:53:29 +0530

    Exercise 1.1: Course Information - Step1; completed

--- part01/part1-step1/src/index.js
+++ part01/part1-step1/src/index.js
@@ -2,28 +2,65 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const course = "Half stack application development"
-  const part1 = "Fundamentals of React"
-  const exercises1 = 10
-  const part2 = "Using props to pass data"
-  const exercises2 = 7
-  const part3 = "State of a component"
-  const exercises3 = 14
+
+  const course = {
+    name: 'Half stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course.name}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  const courseParts = props.course.parts
+  const items = []
+  for(const [index, value] of courseParts.entries()) {
+    items.push(<p key={index}>{value.name} {value.exercises}</p>)
+  }
+  return (
+    <div>
+      { items }
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const courseParts = props.course.parts
+  let total = 0
+
+  courseParts.forEach(function(value, index) {
+    total += value.exercises
+  })
+
+  return (
+    <p>Number of exercises {total}</p>
   )
 }
 
